e1e194be2b234a578a9f1ff33306b109635191ca
Nick Strayer <nick.strayer@gmail.com>
2022-04-05 17:42:08 -0400

    Some more examples of using the json schema to define settings.

--- src/components/Shiny-Ui-Elements/Elements/GridlayoutTextPanel/index.tsx
+++ src/components/Shiny-Ui-Elements/Elements/GridlayoutTextPanel/index.tsx
@@ -16,6 +16,17 @@ export const gridlayoutTextPanelInfo: UiComponentInfo<GridlayoutTextPanelProps> 
     title: "Text Panel",
     UiComponent: GridlayoutTextPanel,
     SettingsComponent: GridlayoutTextPanelSettings,
+    settingsSchema: {
+      content: {
+        type: "string",
+        default: "default title from schema",
+      },
+      h_align: {
+        type: "string",
+        title: "halign",
+        default: "center",
+      },
+    },
     acceptsChildren: false,
     defaultSettings: { content: "Text from Chooser", h_align: "start" },
     iconSrc: textIcon,
--- src/components/Shiny-Ui-Elements/Elements/GridlayoutTitlePanel/index.tsx
+++ src/components/Shiny-Ui-Elements/Elements/GridlayoutTitlePanel/index.tsx
@@ -15,6 +15,17 @@ export const gridlayoutTitlePanelInfo: UiComponentInfo<GridlayoutTitlePanelProps
     title: "Title Panel",
     UiComponent: GridlayoutTitlePanel,
     SettingsComponent: GridlayoutTitlePanelSettings,
+    settingsSchema: {
+      title: {
+        type: "string",
+        default: "default title from schema",
+      },
+      area: {
+        type: "string",
+        title: "Grid Area",
+        default: "unset grid area",
+      },
+    },
     acceptsChildren: false,
     defaultSettings: { title: "Title from Chooser" },
     iconSrc: textIcon,
